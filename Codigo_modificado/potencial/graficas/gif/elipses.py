7e10b6f85a9905dd3b1c8234bc00318aa5a5ece0
Palafox <jdvzzpl@gmail.com>
2023-05-18 07:52:01 -0600

    Update

--- Codigo_modificado/potencial/graficas/gif/elipses.py
+++ Codigo_modificado/potencial/graficas/gif/elipses.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
 N=100
 for i in range(20):
    x,y,a,b,ang=np.loadtxt("gif.dat",skiprows=i*N,max_rows=N,unpack=True)
-   print(x)
